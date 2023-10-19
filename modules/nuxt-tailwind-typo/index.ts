70b6bc2e95474262c23349314cbc21e118be527c
john <aarontu803@gmail.com>
2023-10-19 17:57:13 +0800

    chore:tailwind dev loading before merging

---
+++ modules/nuxt-tailwind-typo/index.ts
@@ -0,0 +1,19 @@
+// ~/modules/nuxt-tailwind-typo/index.ts
+import { defineNuxtModule, addTemplate } from '@nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    name: 'nuxt-tailwind-typo'
+  },
+  setup (options, nuxt) {
+
+    nuxt.hook('tailwindcss:loadConfig', function (loadConfig) {
+      // read: https://tailwindcss.nuxtjs.org/tailwind/editor-support
+      addTemplate({
+        filename: 'tailwind.config.js',
+        getContents: () => `module.exports = ${JSON.stringify(loadConfig)}`,
+        write: true
+      })
+    })
+  }
+})
