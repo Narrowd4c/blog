d5538dc5c8fd7491c5396885cf81aa6f2172ff2c
john <aarontu803@gmail.com>
2023-10-26 16:10:47 +0800

    feat: add article link

--- server/api/article.ts
+++ server/api/article.ts
@@ -3,36 +3,26 @@ export default defineEventHandler((event) => {
     {
       type: "Vue",
       title: "響應式基礎 Ref & Reactive",
-      article: "",
       link: "https://everlasting-hydrangea-e83.notion.site/ref-reactive-4b553ca8484948dc9f0c8069903d504c?pvs=4",
     },
     {
       type: "JavaScript",
-      article: "",
       title: "變數與作用域",
       link: "https://everlasting-hydrangea-e83.notion.site/c228b1d53d7b4c689aa931b90126b4ba?pvs=4",
     },
     {
       type: "CSS",
       title: "用 linear-gradient 做動態邊框",
-      article: "",
       link: "https://codepen.io/narrowd4c/pen/GRPLLeg",
     },
     {
-      type: "TailwindCSS",
-      title: "TailwindCSS Group",
-      link: "#",
-    },
-    {
-      type: "CSS",
-      title: "Scroll-snap 語法介紹",
-      article: "",
-      link: "#",
+      type: "Nuxt",
+      title: "Nuxt Seo 設定",
+      link: "https://everlasting-hydrangea-e83.notion.site/SEO-and-Meta-336453dcd950470b91e87ddfd4f0b764?pvs=4",
     },
     {
       type: "JavaScript",
       title: "JavaScript 型別",
-      article: "",
       link: "https://everlasting-hydrangea-e83.notion.site/196669444fea4eb7b87aa22b52f564ba?pvs=4",
     },
   ];
